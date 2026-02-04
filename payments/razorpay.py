33a80f5e7c0910a45c9fbd983819ebfc21f0e573
sujay5372bot <sujay53721@gmail.com>
2026-02-04 09:03:53 +0530

    Update razorpay.py

--- payments/razorpay.py
+++ payments/razorpay.py
@@ -1,1 +1,12 @@
 #jddj
+import razorpay
+from config import RAZORPAY_KEY, RAZORPAY_SECRET
+
+client = razorpay.Client(auth=(RAZORPAY_KEY, RAZORPAY_SECRET))
+
+def create_link(uid):
+    return client.payment_link.create({
+        "amount": 9900,
+        "currency": "INR",
+        "notes": {"user": uid}
+    })["short_url"]
