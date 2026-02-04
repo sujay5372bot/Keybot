d457da5c4be4996861f44e4af9ffcb9bb753568d
sujay5372bot <sujay53721@gmail.com>
2026-02-04 10:04:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #us
 import os
 
-API_ID = int(os.getenv("24935727))
+API_ID = int(os.getenv("24935727"))
 API_HASH = os.getenv("3fd33336629324ecd664e9b6894f0909")
 BOT_TOKEN = os.getenv("7623238714:AAEBKK1zkm0CvxWkKZjpQ-NNSfZZBD3jpvY")
 
