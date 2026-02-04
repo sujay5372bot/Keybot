26b179b764e7fd7658bfce5854d1bf2b02bd523e
sujay5372bot <sujay53721@gmail.com>
2026-02-04 09:04:16 +0530

    Update stars.py

--- payments/stars.py
+++ payments/stars.py
@@ -1,1 +1,11 @@
 #djjdhd
+def stars_invoice(app, chat_id):
+    return app.send_invoice(
+        chat_id,
+        "Premium",
+        "1 Month Premium",
+        "premium",
+        "",
+        "XTR",
+        [{"label":"Premium","amount":99}]
+    )
