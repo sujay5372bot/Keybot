52de7d73b1c0297079165dc35647ace622597cd0
sujay5372bot <sujay53721@gmail.com>
2026-02-04 10:14:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
 #us
 import os
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+UPI_ID = os.getenv("UPI_ID")
+RAZORPAY_KEY = os.getenv("RAZORPAY_KEY")
+RAZORPAY_SECRET = os.getenv("RAZORPAY_SECRET")
 
-API_ID = int(os.getenv("24935727"))
-API_HASH = os.getenv("3fd33336629324ecd664e9b6894f0909")
-BOT_TOKEN = os.getenv("7623238714:AAEBKK1zkm0CvxWkKZjpQ-NNSfZZBD3jpvY")
+# API_ID = int(os.getenv("24935727"))
+# API_HASH = os.getenv("3fd33336629324ecd664e9b6894f0909")
+# BOT_TOKEN = os.getenv("7623238714:AAEBKK1zkm0CvxWkKZjpQ-NNSfZZBD3jpvY")
 
-ADMIN_ID = int(os.getenv("7336971189"))
-UPI_ID = os.getenv("8371054739.wallet@phonepe")
-RAZORPAY_KEY = os.getenv("rzp_test_SBvuFmKWI1cfhB")
-RAZORPAY_SECRET = os.getenv("OZfl44yjKnyBl9PVpUbXdp6O")
+# ADMIN_ID = int(os.getenv("7336971189"))
+# UPI_ID = os.getenv("8371054739.wallet@phonepe")
+# RAZORPAY_KEY = os.getenv("rzp_test_SBvuFmKWI1cfhB")
+# RAZORPAY_SECRET = os.getenv("OZfl44yjKnyBl9PVpUbXdp6O")
