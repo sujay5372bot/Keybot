0d0d61ed12cd538ea6f6acfa4780644e30b44348
sujay5372bot <sujay53721@gmail.com>
2026-02-04 09:45:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ BOT_TOKEN = os.getenv("7623238714:AAEBKK1zkm0CvxWkKZjpQ-NNSfZZBD3jpvY")
 
 ADMIN_ID = int(os.getenv("7336971189"))
 UPI_ID = os.getenv("8371054739.wallet@phonepe")
-RAZORPAY_KEY = os.getenv("RAZORPAY_KEY")
-RAZORPAY_SECRET = os.getenv("RAZORPAY_SECRET")
+RAZORPAY_KEY = os.getenv("rzp_test_SBvuFmKWI1cfhB")
+RAZORPAY_SECRET = os.getenv("OZfl44yjKnyBl9PVpUbXdp6O")
